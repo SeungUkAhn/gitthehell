c01a51f8e51a1340bc59140c9c81f6913cf7348c
ahn2802 <ahn2802@Mai-Desktop>
2021-09-15 02:55:44 +0900

    this is the second commit message!

---
+++ src/sist/TestWindow2.java
@@ -0,0 +1,34 @@
+package sist;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TestWindow2 extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestWindow2 frame = new TestWindow2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TestWindow2() {
+	}
+
+}
